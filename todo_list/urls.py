5b10aab8d21aa5decef9e09131bfe390d5d51cca
Andrew beattie <muonsales@gmail.com>
2022-01-09 10:58:10 +0800

    Navbar Links Working

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home.html', views.home, name='home'),
+    path('about.html', views.about, name='about'),
 ]
